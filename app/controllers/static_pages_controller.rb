7910e3f17c3b5f9ff2dd052d9a76caf2d2b0e01d
mjahanzeb <malikjahanzeb7@gmail.com>
2014-10-22 13:22:01 -0400

    Completed the book.

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -3,9 +3,6 @@ class StaticPagesController < ApplicationController
     if signed_in?
       @micropost  = current_user.microposts.build
       @feed_items = current_user.feed.paginate(page: params[:page])
-      #if @feed_items == nill
-      #  @feed_items = Micropost.new
-      #end
     end
   end
 
